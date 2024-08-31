3a4ffeda47fd4444bfc6d4d241cb76699cb8ff8e
萌萌的小尸 <142489914+aa48670731@users.noreply.github.com>
2024-08-31 16:53:06 +0800

    Add files via upload

--- 做题记录/求最大子序列和.cpp
+++ 做题记录/求最大子序列和.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
 #include <vector>
+#define MAX(A, B) (A > B ? A : B)
 using namespace std;
-#define MAX(a, b) (a > b ? a : b)
-void find_max_subarray(int n, const vector<int> &array)
-{
-    int max_sum = 0;
-    int current_sum = 0;
-    int start = 0, end = 0, temp_start = 0;
-
-    for (int i = 0; i < n; ++i)
-    {
-        current_sum += array[i];
-
-        if (current_sum > max_sum)
-        {
-            max_sum = current_sum;
-            start = temp_start;
-            end = i;
-        }
-
-        if (current_sum < 0)
-        {
-            current_sum = 0;
-            temp_start = i + 1;
-        }
-    }
-
-    if (max_sum == 0)
-    {
-        cout << "0 -1 -1" << endl;
-    }
-    else
-    {
-        cout << max_sum << " " << start + 1 << " " << end + 1 << endl;
-    }
-}
 
 int findmaxsonarray(int num[], int n)
 {
