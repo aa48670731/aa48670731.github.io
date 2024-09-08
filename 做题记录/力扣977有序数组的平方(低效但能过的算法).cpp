048df1faa18fa9d58d7aba9d9b3753fdfa0fa2a3
萌萌的小尸 <142489914+aa48670731@users.noreply.github.com>
2024-09-08 13:20:02 +0800

    Add files via upload

--- 做题记录/力扣977有序数组的平方(低效但能过的算法).cpp
+++ 做题记录/力扣977有序数组的平方(低效但能过的算法).cpp
@@ -7,12 +7,12 @@ class Solution
 public:
     vector<int> sortedSquares(vector<int> &nums)
     {
-        int n = nums.size();
-        for (int i = 0; i < n; i++)
+        vector<int> ans;
+        for (int num : nums)
         {
-            nums[i] *= nums[i];
+            ans.push_back(num * num);
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
